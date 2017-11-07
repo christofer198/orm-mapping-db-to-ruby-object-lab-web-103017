071a161b87aad98449ebedfd38f27536e50c2e00
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-11-07 19:26:36 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -38,7 +38,7 @@ class Student
     DB[:conn].execute("select * from students where grade=10 limit 1")
 
   end
-  
+
   def self.find_by_name(name)
     # find the student in the database given a name
     # return a new instance of the Student class
