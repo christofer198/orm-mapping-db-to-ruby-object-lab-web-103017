2fd30944931756aca122e12eb85fef21b2a5b4f3
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-11-07 19:14:54 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -21,7 +21,7 @@ class Student
   def self.find_by_name(name)
     # find the student in the database given a name
     # return a new instance of the Student class
-    binding.prt
+    binding.pry
     DB[:conn].execute("select * from students where name='#{}'")
   end
 
