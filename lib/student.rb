7b00cd74a63009350b1e5f9085b46aed2fe29d22
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-11-07 19:32:51 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -37,7 +37,7 @@ class Student
   def self.first_student_in_grade_10
     row = DB[:conn].execute("select * from students where grade=10 limit 1").flatten
     new_student = Student.new
-    binding.pry
+    #binding.pry
     new_student.id = row[0]
     new_student.name = row[1]
     new_student.grade = row[2]
